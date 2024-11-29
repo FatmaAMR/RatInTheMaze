341d5f99fa634dc72151c74e5270019e54157e09
FatmaAMR <fatmamr5102003@gmail.com>
2024-11-29 21:52:12 +0200

    Initial Classes --

--- src/App.java
+++ src/App.java
@@ -3,6 +3,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         char[][] x= Tools.MatrixGenerator(4);
         Tools.MatrixPrinter(x,4);
-        
-            }
+
+    }
 }
